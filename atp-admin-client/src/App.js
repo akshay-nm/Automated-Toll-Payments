0c32dfa8406c0476b1810af7c8634c6ab807175e
Akshay Kumar <akshay.kumar@sdiot.io>
2020-06-24 02:37:10 +0530

    Update App.js

--- atp-admin-client/src/App.js
+++ atp-admin-client/src/App.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import * as ROUTES from './constants/routes';
+import { useSelector } from 'react-redux';
+import Loading from './components/Loading';
+
+const Landing = lazy(() => import('./components/Landing'));
+const LoginOrRegister = lazy(() => import('./components/LoginOrRegister'));
 
 function App() {
+  const isUserLoggedIn = useSelector(state => state.session.isLoggedIn)
+
   return (
-    <div>ATP Admin panel</div>
+    <Suspense fallback={<Loading />}>
+      <Switch>
+        <Route exact path={ROUTES.LANDING} >{isUserLoggedIn? <Landing /> : <Redirect to={ROUTES.LOGIN} />}</Route>
+        <Route path={ROUTES.LOGIN} >{isUserLoggedIn? <Redirect to={ROUTES.LANDING} /> : <LoginOrRegister />}</Route>
+      </Switch>
+    </Suspense>
   );
 }
 
