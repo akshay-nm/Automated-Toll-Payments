befe6d2d50a41b88a47b679cece9be2fddb197ea
Akshay Kumar <akshay.kumar@sdiot.io>
2020-06-23 20:34:45 +0530

    Add and setup tailwind, firebase and reCaptcha

---
+++ atp-admin-client/src/components/Firebase/firebase.js
@@ -0,0 +1,39 @@
+// Firebase
+import * as firebase from 'firebase/app';
+
+import 'firebase/analytics';
+
+import 'firebase/auth';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+};
+
+// Initialize Firebase
+
+class Firebase {
+  constructor() {
+    firebase.initializeApp(firebaseConfig);
+
+    firebase.auth().useDeviceLanguage();
+    this.auth = firebase.auth;
+  }
+
+  // *** Auth API ***
+ 
+  doCreateUserWithEmailAndPassword = (email, password) => this.auth.createUserWithEmailAndPassword(email, password);
+  doSignInWithEmailAndPassword = (email, password) => this.auth.signInWithEmailAndPassword(email, password);
+  doSignOut = () => this.auth.signOut();
+  doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+  doPasswordUpdate = password => this.auth.currentUser.updatePassword(password);
+}
+
+export default Firebase;
--- atp-admin-client/src/index.js
+++ atp-admin-client/src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles/tailwind.css';
 import App from './App';
 import store from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import Firebase, { FirebaseContext } from './components/Firebase';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <FirebaseContext.Provider value={new Firebase()} >
+        <App />
+      </FirebaseContext.Provider>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
