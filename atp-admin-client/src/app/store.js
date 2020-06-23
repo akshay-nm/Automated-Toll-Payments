56b1c002f4d7f81ffd50ab1ee9a399e2d500dbdc
Akshay Kumar <akshay.kumar@sdiot.io>
2020-06-23 21:13:20 +0530

    Add and setupd redux-logger

--- atp-admin-client/src/app/store.js
+++ atp-admin-client/src/app/store.js
@@ -1,8 +1,11 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { logger } from 'redux-logger';
+
 import loginFormDataReducer from './loginFormDataSlice';
 
 export default configureStore({
   reducer: {
     loginFormData: loginFormDataReducer,
   },
+  middleware: [...getDefaultMiddleware(), logger]
 });
