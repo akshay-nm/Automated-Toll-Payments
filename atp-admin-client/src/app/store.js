d3923c8d3e61847e099ca58246fb002218b82c5f
Akshay Kumar <akshay.kumar@sdiot.io>
2020-06-23 21:11:23 +0530

    Add redux state for login form

--- atp-admin-client/src/app/store.js
+++ atp-admin-client/src/app/store.js
@@ -1,6 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import loginFormDataReducer from './loginFormDataSlice';
 
 export default configureStore({
   reducer: {
+    loginFormData: loginFormDataReducer,
   },
 });
